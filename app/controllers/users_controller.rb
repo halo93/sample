d341e86b74f5820f483741f5650e6276c7dc2adf
dohalong <dohalong1993@gmail.com>
2016-10-24 20:54:45 +0700

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = t "welcome"
       redirect_to @user
     else
@@ -26,5 +27,4 @@ class UsersController < ApplicationController
     params.require(:user).permit :name, :email, :password,
                                  :password_confirmation
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  
   resources :users
-
   get "/help", to: "static_pages#help"
-
   get "/about", to: "static_pages#about"
-
   get "/contact", to: "static_pages#contact"
-
   get "/signup", to: "users#new"
-
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   root "static_pages#home"
-
 end
